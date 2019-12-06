d1fa2ac6c59f205aa21b49de4483355840790f68
Ajoymaity <ajoymaity17@gmail.com>
2019-12-06 17:14:04 +0530

    Issue #SB-15505 feat: Topic based notification

--- src/android/MyFirebaseMessagingService.java
+++ src/android/MyFirebaseMessagingService.java
@@ -6,6 +6,7 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
@@ -61,6 +62,40 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         return this.getString(
                 this.getResources().getIdentifier(name, "string", this.getPackageName())
         );
+    }
+
+    public class NotificationUtils extends ContextWrapper {
+        public static final String ANDROID_CHANNEL_ID = "announcement";
+        public static final String ANDROID_CHANNEL_NAME = "Announcement";
+        private NotificationManager mManager;
+
+        public NotificationUtils(Context base) {
+            super(base);
+            createChannels();
+        }
+
+        public void createChannels() {
+            // create android channel
+            NotificationChannel androidChannel = new NotificationChannel(ANDROID_CHANNEL_ID,
+                    ANDROID_CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+            // Sets whether notifications posted to this channel should display notification lights
+            androidChannel.enableLights(true);
+            // Sets whether notification posted to this channel should vibrate.
+            androidChannel.enableVibration(true);
+            // Sets the notification light color for notifications posted to this channel
+            androidChannel.setLightColor(Color.GREEN);
+            // Sets whether notifications posted to this channel appear on the lockscreen or not
+            androidChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+            getManager().createNotificationChannel(androidChannel);
+        }
+
+        private NotificationManager getManager() {
+            if (mManager == null) {
+                mManager = (NotificationManager)
+                        getSystemService(Context.NOTIFICATION_SERVICE);
+            }
+            return mManager;
+        }
     }
 
     /**
